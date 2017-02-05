784b66e689299f747c1dd64e161c2f3d0e8744a1
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-02-05 11:24:54 +0100

    convert to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,116 +1,146 @@
 /**
  * Created by sam on 13.03.2015.
  */
-define(['phovea_core/index', '../caleydo_core/event', '../caleydo_core/src/ajax'],function(C, events, ajax) {
-  'use strict';
+import {hash, param} from 'phovea_core/src';
+import {EventHandler} from 'phovea_core/src/event';
+import {getAPIJSON} from 'phovea_core/src/ajax';
 
-  function ServerSearch() {
-    events.EventHandler.call(this);
-    this._socket = null;
-    this._initialMessage = [];
 
-    this.search_cache = {};
-    this._nodelookup = {};
-    this._rellookup = {};
+export function uc() {
+  return hash.getProp('uc', param.getProp('uc', 'dblp'));
+}
+
+export interface INode {
+  id: string;
+}
+export interface IEdge {
+  id: string;
+}
+
+export interface IPath {
+  nodes: INode[];
+  edges: IEdge[];
+}
+
+export interface IQuery {
+  serialize(): any;
+}
+
+
+function asMessage(type: string, msg: any) {
+  msg.uc = uc();
+  return JSON.stringify({type, data: msg});
+}
+
+export default class ServerSearch extends EventHandler {
+  static readonly EVENT_QUERY_PATH = 'query_path';
+  static readonly EVENT_NEIGHBOR = 'neighbor_neighbor';
+  static readonly EVENT_PATH = 'found';
+
+  private static readonly MSG_NEW_NODE = 'new_node';
+  private static readonly MSG_NEW_EDGE = 'new_relationship';
+  private static readonly SERVER_MSG_QUERY = 'query';
+  private static readonly SERVER_MSG_NEIGBOR = 'neighbor';
+  private static readonly SERVER_MSG_FIND = 'find';
+
+  static readonly EVENT_WS_READY = 'ws_ready';
+  static readonly EVENT_WS_CLOSED = 'ws_closed';
+
+  private socket: WebSocket = null;
+  private readonly initialMessages: string[] = [];
+
+  private readonly searchCache = new Map<string, Map<string, any>>();
+  private readonly nodeLookup = new Map<string, INode>();
+  private readonly edgeLookup = new Map<string, IEdge>();
+
+  resolveConfig() {
+    return getAPIJSON(`/pathways/config.json`, {uc: uc()});
   }
-  C.extendClass(ServerSearch, events.EventHandler);
 
-  function uc() {
-    return C.hash.getProp('uc', C.param.getProp('uc', 'dblp'));
-  }
-
-  ServerSearch.prototype.resolveConfig = function() {
-    return ajax.getAPIJSON('/pathway/config.json?uc='+uc());
-  };
-
-  ServerSearch.prototype.extendPath = function(path) {
-    var that = this;
+  extendPath(path: IPath) {
     return {
-      nodes: path.nodes.map(function(n) { return that.extendNode(n); }),
-      edges: path.edges.map(function(n) { return that.extendRel(n); })
-    }
-  };
-  ServerSearch.prototype.extendNode = function(node) {
-    if (node.id in this._nodelookup) {
-      return this._nodelookup[node.id];
-    }
-    return node;
-  };
-  ServerSearch.prototype.extendRel = function(node) {
-    if (node.id in this._rellookup) {
-      return this._rellookup[node.id];
-    }
-    return node;
-  };
+      nodes: path.nodes.map((n) => this.extendNode(n)),
+      edges: path.edges.map((n) => this.extendRel(n))
+    };
+  }
 
-  ServerSearch.prototype.onMessage = function(msg) {
-    var node;
-    if (msg.type === 'query_path') {
-      msg.data.path = this.extendPath(msg.data.path);
-    } else if (msg.type === 'neighbor_neighbor') {
-      msg.data.neighbor = this.extendNode(msg.data.neighbor);
-      msg.data.edge = msg.data.neighbor._edge = this.extendRel(msg.data.edge);
-    } else if (msg.type === 'found') {
-      var n = this.extendNode(msg.data.node);
-      msg.data.path = {nodes:[n], edges:[]};
-      delete msg.data.node;
-    } else if (msg.type === 'new_node') {
-      node = msg.data;
-      this._nodelookup[node.id] = node;
-      return;
-    } else if (msg.type === 'new_relationship') {
-      node = msg.data;
-      this._rellookup[node.id] = node;
-      return;
+  private extendNode(node: INode) {
+    if (this.nodeLookup.has(node.id)) {
+      return this.nodeLookup.get(node.id);
+    }
+    return node;
+  }
+
+  private extendRel(edge: IEdge) {
+    if (this.edgeLookup.has(edge.id)) {
+      return this.edgeLookup.get(edge.id);
+    }
+    return edge;
+  }
+
+  private onMessage(msg: {type: string, data: any}) {
+    switch (msg.type) {
+      case ServerSearch.EVENT_QUERY_PATH:
+        msg.data.path = this.extendPath(msg.data.path);
+        break;
+      case ServerSearch.EVENT_NEIGHBOR:
+        msg.data.neighbor = this.extendNode(msg.data.neighbor);
+        msg.data.edge = msg.data.neighbor._edge = this.extendRel(msg.data.edge);
+        break;
+      case ServerSearch.EVENT_PATH:
+        const n = this.extendNode(msg.data.node);
+        msg.data.path = {nodes: [n], edges: []};
+        delete msg.data.node;
+        break;
+      case ServerSearch.MSG_NEW_NODE:
+        const node: INode = msg.data;
+        this.nodeLookup.set(node.id, node);
+        return;
+      case ServerSearch.MSG_NEW_EDGE:
+        const edge: IEdge = msg.data;
+        this.edgeLookup.set(edge.id, edge);
+        return;
     }
     this.fire(msg.type, msg.data);
-  };
-  function asMessage(type, msg) {
-    msg.uc = uc();
-    return JSON.stringify({type: type, data: msg});
   }
-  ServerSearch.prototype.send = function(type, msg) {
-    var that = this;
-    if (!this._socket) {
-      that._initialMessage.push(asMessage(type,msg));
 
-      var s = new WebSocket('ws://' + document.domain + ':' + location.port + '/api/pathway/query');
-      that.fire('ws_open');
-      s.onmessage = function (msg) {
-        //console.log('got message', msg);
-        that.onMessage(JSON.parse(msg.data));
+
+  private send(type: string, msg: any) {
+    if (!this.socket) {
+      this.initialMessages.push(asMessage(type, msg));
+      const s = new WebSocket('ws://' + document.domain + ':' + location.port + '/api/pathway/query');
+      this.fire('ws_open');
+      s.onmessage = (msg) => this.onMessage(JSON.parse(msg.data));
+      s.onopen = () => {
+        this.socket = s;
+        this.initialMessages.forEach((msg) => s.send(msg));
+        this.initialMessages.splice(0, this.initialMessages.length);
+        this.fire(ServerSearch.EVENT_WS_READY);
       };
-      s.onopen = function() {
-        that._socket = s;
-        that._initialMessage.forEach(function(msg) {
-          s.send(msg);
-        });
-        that._initialMessage = [];
-        that.fire('ws_ready');
-      };
-      s.onclose = function () {
-        that._socket = null; //clear socket upon close
-        that.fire('ws_closed');
+      s.onclose = () => {
+        this.socket = null; //clear socket upon close
+        this.fire(ServerSearch.EVENT_WS_CLOSED);
       };
 
-    } else if (this._socket.readyState !== WebSocket.OPEN) {
+    } else if (this.socket.readyState !== WebSocket.OPEN) {
       //not yet open cache it
-      that._initialMessage.push(asMessage(type, msg));
+      this.initialMessages.push(asMessage(type, msg));
     } else {
       //send directly
-      this._socket.send(asMessage(type, msg));
+      this.socket.send(asMessage(type, msg));
     }
-  };
+  }
 
-  ServerSearch.prototype.loadQuery = function(query, k, maxDepth, just_network_edges, minLength) {
-    var msg = {
-      k : k || 10,
-      maxDepth : maxDepth || 10,
-      query : query ? query.serialize() : null,
-      just_network_edges : just_network_edges || false,
-      minLength : minLength || 0
+
+  loadQuery(query?: IQuery, k = 10, maxDepth = 10, justNetworkEdges: boolean = false, minLength = 0) {
+    const msg = {
+      k,
+      maxDepth,
+      query: query ? query.serialize() : null,
+      just_network_edges: justNetworkEdges,
+      minLength: minLength || 0
     };
-    this.send('query', msg);
+    this.send(ServerSearch.SERVER_MSG_QUERY, msg);
   };
 
   /**
@@ -118,33 +148,34 @@ define(['phovea_core/index', '../caleydo_core/event', '../caleydo_core/src/ajax'
    * @param query
    * @param k
    */
-  ServerSearch.prototype.find = function(query, k) {
-    var msg = {
-      k : k || 10,
-      query : query ? query.serialize() : null
+  find(query?: IQuery, k = 10) {
+    const msg = {
+      k,
+      query: query ? query.serialize() : null
     };
-    this.send('find', msg);
+    this.send(ServerSearch.SERVER_MSG_FIND, msg);
   };
 
   /**
    *
-   * @param node_id
-   * @param just_network_edges boolean whether just network edges should be considered
+   * @param nodeId
+   * @param justNetworkEdges boolean whether just network edges should be considered
    * @param tag additional tag to transfer, identifying the query
    */
-  ServerSearch.prototype.loadNeighbors = function(node_id, just_network_edges, tag) {
-    var msg = {
-      node : node_id,
-      just_network_edges : just_network_edges || false
+  loadNeighbors(nodeId, justNetworkEdges = false, tag?: any) {
+    const msg = {
+      node: nodeId,
+      just_network_edges: justNetworkEdges,
+      tag: undefined
     };
     if (tag) {
       msg.tag = tag;
     }
-    this.send('neighbor', msg);
+    this.send(ServerSearch.SERVER_MSG_NEIGBOR, msg);
   };
 
-  ServerSearch.prototype.clearSearchCache = function() {
-    this.search_cache = {};
+  clearSearchCache() {
+    this.searchCache.clear();
   };
 
   /**
@@ -153,22 +184,19 @@ define(['phovea_core/index', '../caleydo_core/event', '../caleydo_core/src/ajax'
    * @param prop the property to look in
    * @param nodeType the node type to look in
    */
-  ServerSearch.prototype.search = function(query, prop, nodeType) {
-    prop = prop || 'name';
-    nodeType = nodeType || '_Network_Node';
-    var cache = this.search_cache[nodeType+'.'+prop];
-    if (!cache) {
-      cache = {};
-      this.search_cache[nodeType+'.'+prop] = cache;
+  async search(query: string, prop: string = 'name', nodeType: string = '_Network_Node') {
+    const key = nodeType + '.' + prop;
+    let cache: Map<string, any>;
+    if (!this.searchCache.has(key)) {
+      cache = this.searchCache[nodeType + '.' + prop] = new Map<string, any>();
+    } else {
+      cache = this.searchCache.get(key);
     }
-    if (cache && query in cache) {
-      return Promise.resolve(cache[query]);
+    if (cache.has(query)) {
+      return Promise.resolve(cache.get(query));
     }
-    return ajax.getAPIJSON('/pathway/search', {q: query, prop: prop, label: nodeType, uc : uc()}).then(function (data) {
-      cache[query] = data.results;
-      return data.results;
-    });
+    const data = await getAPIJSON('/pathway/search', {q: query, prop, label: nodeType, uc: uc()});
+    cache.set(query, data.results);
+    return data.results;
   };
-
-  return new ServerSearch();
-});
+}
